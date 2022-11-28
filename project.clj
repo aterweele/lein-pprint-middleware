34cbcc97b05e8571885b499fd326447be5f481ac
Alex ter Weele <alex.ter.weele@gmail.com>
2022-11-28 14:33:56 -0500

    Prepare for Clojars release.

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject nrepl-pprint-middleware "0.1.0-SNAPSHOT"
+(defproject name.atw/nrepl-pprint-middleware "0.1.0-SNAPSHOT"
   :description
   "An nREPL middleware that causes output to be pretty-printed."
   :url "https://github.com/aterweele/lein-pprint-middleware"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [nrepl "0.8.3"]])
+                 [nrepl "0.8.3"]]
+  :deploy-repositories [["clojars" {:username "atw"
+                                    :password :env/deploy_token}]
+                        ["releases" :clojars]
+                        ["snapshots" :clojars]])
