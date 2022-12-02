5640b062b65268e076343ad4f0e41c9bf2b9bfe7
Alex ter Weele <alex.ter.weele@gmail.com>
2022-12-02 12:11:45 -0500

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject name.atw/nrepl-pprint-middleware "0.1.0"
+(defproject name.atw/nrepl-pprint-middleware "0.1.1-SNAPSHOT"
   :description
   "An nREPL middleware that causes output to be pretty-printed."
   :url "https://github.com/aterweele/lein-pprint-middleware"
